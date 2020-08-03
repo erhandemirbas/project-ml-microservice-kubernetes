3619709dca7c8650e5f8e3035904b7b91e4e17f1
EC2 Default User <ec2-user@ip-172-31-26-56.us-west-2.compute.internal>
2020-08-03 14:34:03 +0000

    upload_docker.sh edited

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="erhandemirbas/machine-learning"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag machine-learning $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
