39baeaa086abcd383557c6dd439876f01f8020a7
EC2 Default User <ec2-user@ip-172-31-26-56.us-west-2.compute.internal>
2020-08-03 14:30:24 +0000

    run_kubernetes edited

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="erhandemirbas/machine-learning"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run machineapi\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=machineapi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward machineapi 8000:80
